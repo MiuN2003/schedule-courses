33971e762c610a2806cd1508afe30619efa00725
MiuN2003 <155559862+MiuN2003@users.noreply.github.com>
2024-08-01 10:40:15 +0700

    [Update-push-data] data_preparation.py

--- scripts/data_preparation.py
+++ scripts/data_preparation.py
@@ -1,8 +1,50 @@
 import pandas as pd
+import psycopg2
+
+db_config = {
+'host': 'db-youpass-do-user-15981760-0.c.db.ondigitalocean.com',
+'database': 'drt',
+'user': 'cola',
+'password': 'AVNS_QuPfxcpDcLXuA4V_Dlf',
+'port': '25060'
+}
+
+conn = psycopg2.connect(**db_config)
+cur = conn.cursor()
 
 def load_data():
-    resources = pd.read_csv("resources.csv")
-    courses = pd.read_csv('courses.csv')
-    teachers = pd.read_excel('teachers.xlsx')
+
+    query = "SELECT * FROM public.courses;"
+    cur.execute(query)
+    columns = [desc[0] for desc in cur.description]
+    courses = pd.DataFrame(cur.fetchall(), columns=columns)
+
+    query = "SELECT * FROM public.resources;"
+    cur.execute(query)
+    columns = [desc[0] for desc in cur.description]
+    resources = pd.DataFrame(cur.fetchall(), columns=columns)
+
+    query = "SELECT * FROM public.teachers;"
+    cur.execute(query)
+    columns = [desc[0] for desc in cur.description]
+    teachers = pd.DataFrame(cur.fetchall(), columns=columns)
 
     return resources, courses, teachers
+
+def push_data(results):
+     # Lưu youpass_update vào file CSV
+    results.to_csv('results_optimizer.csv', index=False)
+    
+    # Xóa tất cả các dòng dữ liệu trong bảng
+    delete_query = "DELETE FROM public.results_optimizer;"
+    cur.execute(delete_query)
+    conn.commit()
+    
+    # Nhập dữ liệu từ file CSV vào PostgreSQL
+    copy_sql = """
+        COPY public.results_optimizer FROM stdin WITH CSV HEADER
+        DELIMITER as ','
+    """
+    with open('results_optimizer.csv', 'r', encoding='utf-8') as f:
+        cur.copy_expert(sql=copy_sql, file=f)
+    conn.commit()
